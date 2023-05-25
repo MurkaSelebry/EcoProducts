273ba810ada61f3acc7c2629380f3eedb757f705
Igor Slyusar <109157467+NotReptile@users.noreply.github.com>
2023-05-25 23:55:30 +0300

    базовая часть бота

---
+++ TelegaBot/Bot.py
@@ -0,0 +1,16 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('5932020939:AAFC56lTYXkvbWdARlxKHlvP4fqlHn_-AoQ')
+
+@bot.message_handler(commands=['start','help'])
+def start(message):
+    mess = f'Привет, <b>{message.from_user.first_name}</b>!\nЯ //какой-то бот//'
+    bot.send_message(message.chat.id, mess, parse_mode='html')
+    bot.send_message(message.chat.id,'Напиши <strong>/info</strong> чтобы узнать больше обо мне ', parse_mode='html')
+
+@bot.message_handler(commands=['info', 'Info', 'INFO'])
+def info(message):
+    bot.send_message(message.chat.id, '''Я умею подбирать продукты и //что-то еще// ''', parse_mode='html')
+
+bot.polling(none_stop=True)
