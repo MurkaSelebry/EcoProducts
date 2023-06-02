c770e80f1e4238c10a456aeadf3223aa679d1ecc
selebry <murad1703@yandex.ru>
2023-06-02 14:44:25 +0300

    add documentation

--- TelegaBot/Bot.py
+++ TelegaBot/Bot.py
@@ -1,5 +1,8 @@
+import threading
+
 import telebot
 from telebot import types
+from AI.test import Operations
 
 bot = telebot.TeleBot('5932020939:AAFC56lTYXkvbWdARlxKHlvP4fqlHn_-AoQ')
 
@@ -21,6 +24,7 @@ regular_products = {
     'Яйца': 25
 }
 
+op = Operations()
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
@@ -47,6 +51,8 @@ def process_price_input(message):
     chat_id = message.chat.id
     product_prices[chat_id] = product_name
     bot.reply_to(message, f"Вы выбрали продукт: {product_name}")
+    bot.send_message(message.chat.id, op.getPrice(product_name))
+
 
 
 # обработка выбранного магазина после того как чел нажимает кнопку Магазины
@@ -55,6 +61,8 @@ def process_store_input(message):
     chat_id = message.chat.id
     selected_stores[chat_id] = store_name
     bot.reply_to(message, f"Вы выбрали магазин: {store_name}")
+    bot.send_message(message.chat.id, op.setStore(store_name))
+
 
 
 # обработка выбранного плана после того как чел нажимает кнопку План
@@ -63,6 +71,7 @@ def process_plan_input(message):
     chat_id = message.chat.id
     selected_plans[chat_id] = plan_duration
     bot.reply_to(message, f"Вы выбрали продолжительность плана: {plan_duration}")
+    bot.send_message(message.chat.id, op.getSchedule(plan_duration))
 
 
 @bot.message_handler(func=lambda message: message.text == 'Рацион питания')
@@ -84,14 +93,10 @@ def handle_price(message):
 @bot.message_handler(func=lambda message: message.text == 'Магазины')
 def handle_stores(message):
     markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-    button1 = types.KeyboardButton('Евроспар')
-    button2 = types.KeyboardButton('АШАН')
-    button3 = types.KeyboardButton('Верный')
-    button4 = types.KeyboardButton('METRO')
-    button5 = types.KeyboardButton('Перекресток')
-    button6 = types.KeyboardButton('Пятерочка')
+    for el in op.store.keys():
+        markup.add(types.KeyboardButton(el))
     back_button = types.KeyboardButton('Назад')
-    markup.add(button1, button2, button3, button4, button5, button6, back_button)
+    markup.add( back_button)
     bot.reply_to(message, "Выберите название ближайшего к вам магазина:", reply_markup=markup)
     bot.register_next_step_handler(message, process_store_input)
 
@@ -111,30 +116,46 @@ def handle_plan(message):
 @bot.message_handler(func=lambda message: message.text == 'Хочу кушать')
 def handle_hungry(message):
     markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-    button1 = types.KeyboardButton('Веганский')
-    button2 = types.KeyboardButton('Обычный')
+    button1 = types.KeyboardButton('Веган')
+    button2 = types.KeyboardButton('Любой')
     back_button = types.KeyboardButton('Назад')
     markup.add(button1, button2, back_button)
-    bot.reply_to(message, "Выберите рацион:", reply_markup=markup)
+    bot.reply_to(message, "Какой продукт вы хотите:", reply_markup=markup)
 
+@bot.message_handler(func=lambda message: message.text == 'Веган')
+def handle_regular_ration(message):
+    bot.reply_to(message, f"Веган продукт:")
+    bot.send_message(message.chat.id, op.getRandomProduct(True))
 
+@bot.message_handler(func=lambda message: message.text == 'Любой')
+def handle_regular_ration(message):
+    bot.reply_to(message, f"Обычный продукт:")
+    bot.send_message(message.chat.id, op.getRandomProduct())
 @bot.message_handler(func=lambda message: message.text == 'Веганский')
 def handle_vegan_ration(message):
     bot.reply_to(message, "Продукты веганского рациона:")
-    for product, price in vegan_products.items():
-        bot.send_message(message.chat.id, f"{product} - {price} рублей")
 
+    def get_diet_and_send_message():
+        diet = op.getDiet(True)
+        bot.send_message(message.chat.id, diet)
 
+    threading.Thread(target=get_diet_and_send_message).start()
 @bot.message_handler(func=lambda message: message.text == 'Обычный')
 def handle_regular_ration(message):
-    bot.reply_to(message, "Продукты обычного рациона:")
-    for product, price in regular_products.items():
-        bot.send_message(message.chat.id, f"{product} - {price} рублей")
+    bot.reply_to(message, f"Продукты обычного рациона:")
+    bot.send_message(message.chat.id, op.getDiet())
+
 
 
 @bot.message_handler(func=lambda message: message.text == 'Назад')
 def handle_back(message):
     start(message)
 
+# Handle all other messages.
+@bot.message_handler(func=lambda message: True, content_types=['audio', 'photo', 'voice', 'video', 'document',
+    'text', 'location', 'contact', 'sticker'])
+def default_command(message):
+    bot.send_message(message.chat.id, "Такой команды не существует")
+    start(message)
 
 bot.polling(none_stop=True)
